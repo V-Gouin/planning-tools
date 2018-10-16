b39b15c66150184154f36e0c04f7b23cdec8af01
Victor-G <gouinvictor@gmail.com>
2018-10-16 23:18:22 +0200

    class ElectricalNetwork

---
+++ planning-tools/electrical_network/electrical_network.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class ElectricalNetwork:
+
+    def __init__(self, buses, branches):
+
+        self.buses = buses
+        self.branches = branches
